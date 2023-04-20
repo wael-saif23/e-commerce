cfa39a2e9f5757b7a09fc5a23168a74befad92b8
wael4code <121891714+wael4code@users.noreply.github.com>
2023-04-20 15:10:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+
+
+import 'package:e_commerce_app/pages/homr.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
---
+++ lib/models/items_model.dart
@@ -0,0 +1,19 @@
+class Item {
+  final double price;
+  final String imgPath;
+
+  Item({required this.price,required this.imgPath});
+
+}
+
+
+final List<Item> items = [
+    Item(price: 12.99, imgPath: "assets/img/1.webp"),
+    Item(price: 12.99, imgPath: "assets/img/2.webp"),
+    Item(price: 12.99, imgPath: "assets/img/3.webp"),
+    Item(price: 12.99, imgPath: "assets/img/4.webp"),
+    Item(price: 12.99, imgPath: "assets/img/5.webp"),
+    Item(price: 12.99, imgPath: "assets/img/6.webp"),
+    Item(price: 12.99, imgPath: "assets/img/7.webp"),
+    Item(price: 12.99, imgPath: "assets/img/8.webp"),
+ ];
