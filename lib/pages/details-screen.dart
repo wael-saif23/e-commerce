9f523f694ba60835dfbac33c719a7f5e8805b51c
wael4code <121891714+wael4code@users.noreply.github.com>
2023-04-23 17:08:22 +0200

    add details screen

---
+++ lib/pages/details-screen.dart
@@ -0,0 +1,173 @@
+import 'package:e_commerce_app/shared/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+ bool  isShowMore = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      actions: [
+        Row(
+          children: [
+            Stack(
+              children: [
+                Positioned(
+                  bottom: 24,
+                  child: Container(
+                       padding: const EdgeInsets.all(5),
+                      decoration: const BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
+                       child: const Text(
+                        "8",
+                        style: TextStyle(
+                            fontSize: 16, color: Color.fromARGB(255, 0, 0, 0)),
+                      )),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add_shopping_cart),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(right: 12),
+              child: Text(
+                "\$ 13",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ],
+      backgroundColor: appbarGreen,
+      title: const Text("Details Screen"),
+    ),
+        body: Column(
+          children: [
+             Image.asset("assets/img/2.webp"),
+              SizedBox(
+                height: 11,
+              ),
+              Text(
+                "\$ 12.99",
+                style: TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      padding: EdgeInsets.all(4),
+                      child: Text(
+                        "New",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 255, 129, 129),
+                        borderRadius: BorderRadius.circular(4),
+                      )),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                         size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 66,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.edit_location,
+                        size: 26,
+                        color: Color.fromARGB(168, 3, 65, 27),
+                        // color: Color.fromARGB(255, 186, 30, 30),
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "Flower Shop",
+                        style: TextStyle(fontSize: 19),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "Details : ",
+                  style: TextStyle(fontSize: 22),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "A flower, sometimes known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). The biological function of a flower is to facilitate reproduction, usually by providing a mechanism for the union of sperm with eggs. Flowers may facilitate outcrossing (fusion of sperm and eggs from different individuals in a population) resulting from cross-pollination or allow selfing (fusion of sperm and egg from the same flower) when self-pollination occurs.",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                maxLines: isShowMore ? 3 : null,
+                overflow: TextOverflow.fade,
+              ),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      isShowMore = !isShowMore;
+                    });
+                  },
+                  child: Text(
+                    isShowMore ? "Show more" : "Show less",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ))),
+          ],
+        ),
+    );
+  }
+}
