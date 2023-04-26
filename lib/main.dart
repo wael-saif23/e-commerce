7348977d616566fb632c70dc06f95917477c5ef4
wael4code <121891714+wael4code@users.noreply.github.com>
2023-04-27 01:59:51 +0200

    add firebase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:e_commerce_app/pages/home.dart';
 import 'package:e_commerce_app/providers/cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
